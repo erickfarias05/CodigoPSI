db7036c90baa32e564c3a342cedc2eafd52614cb
20201011110027 <20201011110027@CNAT391775>
2022-11-17 11:44:43 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 0505/Controllers/CategoriasController.cs
@@ -0,0 +1,116 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Modelo.Tabelas;
+using Servico.Tabelas;
+using System;
+
+namespace PSI.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        //public EFContext context = new EFContext();
+        //public ActionResult HtpNotFoundResult { get; private set; }
+
+        private CategoriaServico categoriaServico = new CategoriaServico();
+
+        private ActionResult ObterVisaoCategoriaId(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(
+                HttpStatusCode.BadRequest);
+            }
+            Categoria fabricante = categoriaServico.ObterCategoriaPorId((long)id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        private ActionResult GravarCategoria(Categoria categoria)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    categoriaServico.GravarCategoria(categoria);
+                    return RedirectToAction("Index");
+                }
+                return View(categoria);
+            }
+            catch
+            {
+                return View(categoria);
+            }
+        }
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categoriaServico.ObterCategoriasClassificadasPorNome());
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            return GravarCategoria(categoria);
+        }
+
+        // GET: Edit
+        public ActionResult Edit(long? id)
+        {
+            return ObterVisaoCategoriaId(id);
+        }
+
+
+        // POST: Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            return GravarCategoria(categoria);
+        }
+
+
+        // GET: Details
+        public ActionResult Details(long? id)
+        {
+            return ObterVisaoCategoriaId(id);
+        }
+
+        // GET: Delete
+        public ActionResult Delete(long? id)
+        {
+            return ObterVisaoCategoriaId(id);
+        }
+
+
+        // POST: Categorias/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            try
+            {
+                Categoria categoria = categoriaServico.EliminarCategoriaPorId(id);
+                TempData["Message"] = "Categoria " + categoria.Nome.ToUpper() + " foi removida";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
